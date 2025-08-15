e8df904d9b7c4b5bf11605adcbc2dac66872c7f6
Kazue Kurosaki <137025030+kazuekurosaki@users.noreply.github.com>
2025-08-16 02:49:07 +0700

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -2,9 +2,9 @@ import type { Config } from './types';
 
 export default {
   id: '/',
-  name: 'My Blog',
-  shortName: 'My Blog',
-  description: 'My blog description goes here...',
+  name: 'Infinite Novel Translations',
+  shortName: 'INT',
+  description: 'Experience an infinite array of translated light novels and web novels with Infinite Novel Translations. Enjoy up-to-date reviews, reading guides, and popular series.',
   direction: 'auto',
   language: 'en-US',
   backgroundColor: '#fff',
@@ -17,27 +17,27 @@ export default {
   preferRelatedApplications: false,
   shortcuts: [
     {
-      name: 'Shortcut 1',
-      shortName: 'Shortcut 1',
-      description: 'Shortcut 1 description goes here...',
-      url: '/search/label/shortcut-1?utm_source=homescreen',
+      name: 'List Novel',
+      shortName: 'List',
+      description: 'Daftar Novel Yang Sudah Di Up',
+      url: '/p/list.html?utm_source=homescreen',
     },
     {
-      name: 'Shortcut 2',
-      shortName: 'Shortcut 2',
-      description: 'Shortcut 2 description goes here...',
-      url: '/search/label/shortcut-2?utm_source=homescreen',
+      name: 'About Us',
+      shortName: 'Tentang Kami',
+      description: 'Tentang Kami Sebagai Penerjemah',
+      url: '/p/about-us.html?utm_source=homescreen',
     },
   ],
   pwa: {
     logs: true,
     // OneSignal is only available if you are using cloudflare workers
-    oneSignalEnabled: false, // To enable OneSignal, set this to true
+    oneSignalEnabled: true, // To enable OneSignal, set this to true
     oneSignalConfig: {
-      appId: '********-****-****-****-************', // Replace with your OneSignal App Id
+      appId: 'd77b7040-d52b-4e6a-829b-26cece87604d', // Replace with your OneSignal App Id
       allowLocalhostAsSecureOrigin: true,
     },
   },
   // Please replace with your blog url if you are using CDN (JsDelivr)
-  origin: 'https://hello-example.blogspot.com',
+  origin: 'https://infinitenoveltranslations.blogspot.com',
 } satisfies Config;
